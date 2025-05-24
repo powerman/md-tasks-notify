f4c8962b3c9b22f2d50ed858246b4725eebbf852
Alex Efros <powerman@powerman.name>
2025-05-24 10:26:39 +0300

    chore: add tests

--- main.go
+++ main.go
@@ -2,11 +2,9 @@
 package main
 
 import (
-	"bytes"
+	"io"
 	"log"
 	"os"
-	"text/template"
-	"time"
 
 	obsidian "github.com/powerman/goldmark-obsidian"
 	"github.com/yuin/goldmark"
@@ -14,76 +12,22 @@ import (
 	"github.com/yuin/goldmark/util"
 )
 
-// getExampleMarkdown returns example Markdown content with dates relative to current date using Go templates.
-func getExampleMarkdown() []byte {
-	type markdownData struct {
-		Yesterday string
-		Today     string
-		Tomorrow  string
-	}
-
-	const markdownTmpl = `---
-a: 1
-b: x
-  - b 2
-  - [ ] b 3
----
-
-# Test
-
-- Item _cool_ 1
-- [ ] Due yesterday 📅 {{.Yesterday}}
-- [ ] Due today     📅 {{.Today}}
-- [ ] Due tomorrow  📅 {{.Tomorrow}}
-- [ ] Scheduled yesterday ⏳ {{.Yesterday}}
-- [ ] Scheduled today     ⏳ {{.Today}}
-- [ ] Scheduled tomorrow  ⏳ {{.Tomorrow}}
-- [ ] Start yesterday 🛫 {{.Yesterday}}
-- [ ] Start today     🛫 {{.Today}}
-- [ ] Start tomorrow  🛫 {{.Tomorrow}}
-- [ ] Due tomorrow Start yesterday 🛫 {{.Yesterday}} 📅 {{.Tomorrow}}
-- [ ] Due tomorrow Start today     🛫 {{.Today}} 📅 {{.Tomorrow}}
-- [ ] Due tomorrow Start tomorrow  🛫 {{.Tomorrow}} 📅 {{.Tomorrow}}
-- [ ] Scheduled tomorrow Start yesterday 🛫 {{.Yesterday}} ⏳ {{.Tomorrow}}
-- [ ] Scheduled tomorrow Start today     🛫 {{.Today}} ⏳ {{.Tomorrow}}
-- [ ] Scheduled tomorrow Start tomorrow  🛫 {{.Tomorrow}} ⏳ {{.Tomorrow}}
-- [X] Task
-  - [ ] Subtask
-- [x] Large _cool_ real task 🆔 jps5k3 #tag ⛔ peg74d,gg3xkn ⏬ 🔁 every day ➕ 2024-10-15 🛫 2024-10-15 ⏳ 2024-10-15 📅 2024-10-15 ❌ 2024-10-15 ✅ 2024-10-15 ^some-id
-- [ ] Task _2 📅2024-10-19 📅2024-10-15
-  🔼⏬_ #tag ^e5bebf
-
-  Second paragraph ⏬
-  - [ ] Second line.`
-
-	today := time.Now()
-	data := markdownData{
-		Yesterday: today.AddDate(0, 0, -1).Format(time.DateOnly),
-		Today:     today.Format(time.DateOnly),
-		Tomorrow:  today.AddDate(0, 0, 1).Format(time.DateOnly),
-	}
-
-	tmpl, err := template.New("markdown").Parse(markdownTmpl)
-	if err != nil {
-		panic(err)
-	}
-
-	var buf bytes.Buffer
-	err = tmpl.Execute(&buf, data)
-	if err != nil {
-		panic(err)
-	}
-
-	return buf.Bytes()
-}
-
 // TODO: Поддержка флагов -email, -from-day, -to-day.
 // TODO: Чтение файлов из os.Args (либо stdin, если файлов нет).
 // TODO: Вывод с указанием из какого файла эти задачи (если есть хоть одна из этого файла).
 // TODO: Отправка вывода на email либо stdout (если -email не задан).
 func main() {
-	source := getExampleMarkdown()
+	data, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = run(data, os.Stdout)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
+func run(source []byte, w io.Writer) error {
 	md := goldmark.New(
 		goldmark.WithExtensions(
 			obsidian.NewPlugTasks(),
@@ -94,8 +38,6 @@ func main() {
 			util.Prioritized(NewActualTasksRenderer(1, 1), 0),
 		)),
 	)
-	err := md.Convert(source, os.Stdout)
-	if err != nil {
-		log.Fatal(err)
-	}
+	err := md.Convert(source, w)
+	return err
 }
---
+++ main_test.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+	"text/template"
+	"time"
+)
+
+// getExampleMarkdown returns example Markdown content with dates relative to current date using Go templates.
+func getExampleMarkdown() []byte {
+	type markdownData struct {
+		Yesterday string
+		Today     string
+		Tomorrow  string
+	}
+
+	const markdownTmpl = `---
+a: 1
+b: x
+  - b 2
+  - [ ] b 3
+---
+
+# Test
+
+- Item _cool_ 1
+- [ ] Due yesterday 📅 {{.Yesterday}}
+- [ ] Due today     📅 {{.Today}}
+- [ ] Due tomorrow  📅 {{.Tomorrow}}
+- [ ] Scheduled yesterday ⏳ {{.Yesterday}}
+- [ ] Scheduled today     ⏳ {{.Today}}
+- [ ] Scheduled tomorrow  ⏳ {{.Tomorrow}}
+- [ ] Start yesterday 🛫 {{.Yesterday}}
+- [ ] Start today     🛫 {{.Today}}
+- [ ] Start tomorrow  🛫 {{.Tomorrow}}
+- [ ] Due tomorrow Start yesterday 🛫 {{.Yesterday}} 📅 {{.Tomorrow}}
+- [ ] Due tomorrow Start today     🛫 {{.Today}} 📅 {{.Tomorrow}}
+- [ ] Due tomorrow Start tomorrow  🛫 {{.Tomorrow}} 📅 {{.Tomorrow}}
+- [ ] Scheduled tomorrow Start yesterday 🛫 {{.Yesterday}} ⏳ {{.Tomorrow}}
+- [ ] Scheduled tomorrow Start today     🛫 {{.Today}} ⏳ {{.Tomorrow}}
+- [ ] Scheduled tomorrow Start tomorrow  🛫 {{.Tomorrow}} ⏳ {{.Tomorrow}}
+- [X] Task
+  - [ ] Subtask
+- [x] Large _cool_ real task 🆔 jps5k3 #tag ⛔ peg74d,gg3xkn ⏬ 🔁 every day ➕ 2024-10-15 🛫 2024-10-15 ⏳ 2024-10-15 📅 2024-10-15 ❌ 2024-10-15 ✅ 2024-10-15 ^some-id
+- [ ] Task _2 📅2024-10-19 📅2024-10-15
+  🔼⏬_ #tag ^e5bebf
+
+  Second paragraph ⏬
+  - [ ] Second line.`
+
+	today := time.Now()
+	data := markdownData{
+		Yesterday: today.AddDate(0, 0, -1).Format(time.DateOnly),
+		Today:     today.Format(time.DateOnly),
+		Tomorrow:  today.AddDate(0, 0, 1).Format(time.DateOnly),
+	}
+
+	tmpl, err := template.New("markdown").Parse(markdownTmpl)
+	if err != nil {
+		panic(err)
+	}
+
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, data)
+	if err != nil {
+		panic(err)
+	}
+
+	return buf.Bytes()
+}
+
+func TestRun(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []byte
+		wantErr  bool
+		contains []string // Strings that should be present in output
+		excludes []string // Strings that should not be present in output
+	}{
+		{
+			name:  "Example markdown",
+			input: getExampleMarkdown(),
+			contains: []string{
+				"Due tomorrow",                       // Should contain tasks due tomorrow
+				"Scheduled tomorrow",                 // Should contain tasks scheduled for tomorrow
+				"Due tomorrow Start yesterday",       // Should contain tasks starting before today with due date tomorrow
+				"Due tomorrow Start today",           // Should contain tasks starting today with due date tomorrow
+				"Scheduled tomorrow Start yesterday", // Should contain tasks starting before today with scheduled date tomorrow
+				"Scheduled tomorrow Start today",     // Should contain tasks starting today with scheduled date tomorrow
+			},
+			excludes: []string{
+				"---",                               // Should not contain frontmatter
+				"# Test",                            // Should not contain headers
+				"Item _cool_ 1",                     // Should not contain non-task items
+				"Due yesterday",                     // Should not contain past tasks
+				"Due today",                         // Should not contain today's tasks
+				"[X] Task",                          // Should not contain completed tasks
+				"[x] Large _cool_ real task",        // Should not contain completed tasks (lowercase x)
+				"Due tomorrow Start tomorrow",       // Should not contain tasks starting tomorrow
+				"Scheduled tomorrow Start tomorrow", // Should not contain tasks starting tomorrow
+				"Task _2",                           // Should not contain tasks with dates far in future
+			},
+			wantErr: false,
+		},
+		{
+			name:    "Empty input",
+			input:   []byte{},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var buf bytes.Buffer
+			err := run(tt.input, &buf)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("run() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			output := buf.String()
+			for _, want := range tt.contains {
+				if !strings.Contains(output, want) {
+					t.Errorf("run() output should contain %q but doesn't\nOutput:\n%s", want, output)
+				}
+			}
+			for _, exclude := range tt.excludes {
+				if strings.Contains(output, exclude) {
+					t.Errorf("run() output should not contain %q but does\nOutput:\n%s", exclude, output)
+				}
+			}
+		})
+	}
+}
